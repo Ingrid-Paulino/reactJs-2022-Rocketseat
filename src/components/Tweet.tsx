b17c65f0b1b3fe2cb2755f66288c06750cebe41d
Ingrid-Paulino <ingridpaulino1012@gmail.com>
2022-04-01 12:50:17 -0300

    Criado component e props

---
+++ src/components/Tweet.tsx
@@ -0,0 +1,18 @@
+// Leia: eu que definir uma tipagem para as propriedades do meu Tweet
+type TweetProps = {
+  text: string; // text esta de forma obrigatoria
+  // text?: string; // text esta de forma opsional
+
+}
+
+export function Tweet(props: TweetProps) {
+  // quando o html tem mais de uma linha, nós colocamos parentese
+  return (
+    <div>
+      <p>Tweet</p>
+      {/* Para usar js com html é necessario o uso das chaves */}
+      <p>{props.text}</p>
+    </div>
+    
+  )
+}
