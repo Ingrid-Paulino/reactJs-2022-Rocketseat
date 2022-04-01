17d20b1190caee9117f380a6551e8a421f7c76ed
Ingrid-Paulino <ingridpaulino1012@gmail.com>
2022-04-01 13:04:26 -0300

    adicionado estilizaçao css

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react"
 import { Tweet } from "./components/Tweet"
 
+import './App.css';
+
 function App() {
   // useState cria uma variável que é monitorada pelo compnente, ou seja, toda vez que essa variável muda o seu valo, o componente é remontado/renderizado com as novas informaçoes daquela variável
   // O que eu passo dentro do () é o valor inicial do meu estado
